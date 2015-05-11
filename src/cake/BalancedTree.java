391fd5d746d6279306ee3ff6d9e8ccef26049486
himanshuy <himsonly@gmail.com>
2015-05-11 12:33:26 -0400

    Check if Binary Tree is Balanced

---
+++ src/cake/BalancedTree.java
@@ -0,0 +1,20 @@
+package cake;
+
+import util.TreeNode;
+
+public class BalancedTree {
+	
+	public static boolean isBalanced(TreeNode root) {
+		if(root == null) return true;
+		
+		return Math.max(getHeight(root.left),getHeight(root.right)) > 1;
+	}
+
+	private static int getHeight(TreeNode node) {
+		
+		if(node == null) return 0;
+		 
+		return Math.abs(getHeight(node.left) - getHeight(node.right)) +1;
+	}
+
+}
