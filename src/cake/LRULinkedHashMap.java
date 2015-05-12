c806f8547c983e9431a16379420bb425761eecaf
himanshuy <himsonly@gmail.com>
2015-05-12 13:01:33 -0400

    LRU implementation with LinkedHashMap

---
+++ src/cake/LRULinkedHashMap.java
@@ -0,0 +1,17 @@
+package cake;
+
+import java.util.LinkedHashMap;
+
+public class LRULinkedHashMap extends LinkedHashMap<Integer, String>{
+	
+	private int capacity;
+	
+	public LRULinkedHashMap(int size) {
+		super(size, 0.75f, true);
+		this.capacity = size;
+	}
+
+	protected boolean removeEldestEntry(java.util.Map.Entry<Integer, String> entry) {
+		return size() > capacity;
+	}
+}
