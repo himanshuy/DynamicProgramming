444662b5b705bc156dc288e26a33d8ac475a44d8
himanshuy <himsonly@gmail.com>
2015-05-17 21:30:36 -0400

    Binary Search On Sorted Array

---
+++ src/cake/NumberInSortedArray.java
@@ -0,0 +1,31 @@
+package cake;
+
+public class NumberInSortedArray {
+
+	public static void main(String[] args) {
+		int[] arr = {1, 2, 3, 4, 5};
+		System.out.println(findNumber(arr,3));
+	}
+	
+	public static boolean findNumber(int[] arr, int num) {
+		boolean result = false;
+		
+		int firstNumber = 0;
+		
+		int mid = arr.length/2;
+		
+		while(mid<arr.length) {
+			if(arr[mid] > num) {
+				mid = (firstNumber + (mid-1))/2;
+			} else if(arr[mid] < num) {
+				firstNumber = mid;
+				mid = ((firstNumber+1)+arr.length)/2;
+			} else {
+				return true;
+			}
+		}
+		
+		return result;
+	}
+
+}
